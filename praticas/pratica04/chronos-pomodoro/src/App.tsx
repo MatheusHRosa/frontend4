a2e85de9c5d071b002eab9286e5d60d7d90331eb
MatheusHRosa <matheus.silva19@iesb.edu.br>
2026-03-20 21:31:31 -0300

    atv3

---
+++ praticas/pratica04/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1>Olá, Mundo do React!</h1>;
+}
---
+++ praticas/pratica04/chronos-pomodoro/src/main.tsx
@@ -0,0 +1,6 @@
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+import { App } from './App';
