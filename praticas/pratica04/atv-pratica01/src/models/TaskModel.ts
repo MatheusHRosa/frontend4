ebe5a7e9537af8520d492dd86fb9f31019490760
MatheusHRosa <matheus.silva19@iesb.edu.br>
2026-04-02 21:31:38 -0300

    feat: atualizando ate a feature 30

---
+++ praticas/pratica04/atv-pratica01/src/models/TaskModel.ts
@@ -0,0 +1,9 @@
+import type { TaskStateModel } from './TaskStateModel';
+
+export type TaskModel = {
+  id: string; // Identificador único da tarefa
+  name: string; // Nome digitado no input
+  duration: number; // Duração em minutos
+  startDate: number; // Timestamp de quando começou (usamos number para facilitar o localStorage)
+  complete:boolean;
+  state: TaskStateModel;}
